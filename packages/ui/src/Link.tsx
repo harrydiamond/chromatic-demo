a8ccc07f8775fc21f85c17103be6eb080b675d14
Harry Diamond <hdiamond@bunnings.com.au>
2023-07-16 18:38:04 +1000

    feat: add link component

---
+++ packages/ui/src/Link.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import { AnchorHTMLAttributes } from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
+
+const linkVariants = cva('block', {
+  variants: {
+    size: {
+      sm: 'text-sm',
+      md: 'text-md',
+      lg: 'text-lg',
+      xl: 'text-xl',
+    },
+    color: {
+      black: 'text-black',
+      red: 'text-red-500',
+      green: 'text-green-500',
+    },
+  },
+});
+
+export type LinkProps = AnchorHTMLAttributes<HTMLAnchorElement> &
+  VariantProps<typeof linkVariants> & {
+    children: React.ReactNode;
+  };
+
+export const Link = ({ size, color, ...rest }: LinkProps) => (
+  <a className={linkVariants({ size, color })} {...rest} />
+);
